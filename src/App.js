f09f57a0fa949b90984722fb5be647a4d59da197
Shailesh Dhokare <GS-2450@GSLAB.COM>
2020-03-12 11:00:03 +0530

    project setup-building ecommerce app in react and firebase

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Homepage from "./pages/homepage/Homepage";
+
+function App() {
+	return (
+		<div className="container">
+			<header className="App-header">
+            <h1>E-commerce store</h1>
+            <Homepage />
+         </header>
+		</div>
+	);
+}
+
+export default App;
